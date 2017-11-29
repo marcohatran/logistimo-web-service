47fcd5c2bbafa0145d351886f4614719d44ea1fb
naveensnair-logistimo <naveen@logistimo.com>
2017-11-29 12:57:07 +0530

    Ls 6424 (#506)
    * donut fixes and alignments
    
    * event widgets
    
    * trend widget

---
+++ modules/web/src/main/webapp/v2/plugins/storyboards/events/events-map-widget/index.js
@@ -0,0 +1,148 @@
+/**
+ * Created by yuvaraj on 10/11/17.
+ */
+angular.module('logistimo.storyboard.eventMapWidget', [])
+    .config(function (widgetsRepositoryProvider) {
+        widgetsRepositoryProvider.addWidget({
+            id: "eventMapWidget",
+            name: "Event map",
+            templateUrl: "plugins/storyboards/events/event-map-widget/events/event-map-widget-widget.html",
+            editTemplateUrl: "plugins/storyboards/events/edit-template.html",
+            templateFilters: [
+                {
+                    nameKey: 'inventory.status',
+                    type: 'mapType'
+                },
+                {
+                    nameKey: 'material.upper',
+                    type: 'material'
+                },
+                {
+                    nameKey: 'filter.material.tag',
+                    type: 'materialTag'
+                },
+                {
+                    nameKey: 'include.entity.tag',
+                    type: 'entityTag'
+                },
+                {
+                    nameKey: 'exclude.entity.tag',
+                    type: 'exEntityTag'
+                },
+                {
+                    nameKey: 'date',
+                    type: 'date'
+                },
+                {
+                    nameKey: 'period.since',
+                    type: 'period'
+                }
+            ],
+            defaultHeight: 4,
+            defaultWidth: 5
+        });
+    })
+    .controller('eventMapWidgetController',
+    ['$scope', '$timeout', 'dashboardService', 'domainCfgService', 'INVENTORY', '$sce',
+        function ($scope, $timeout, dashboardService, domainCfgService, INVENTORY, $sce) {
+            var filter = angular.copy($scope.widget.conf);
+            var invPieColors, invPieOrder, mapRange, mapColors;
+            var fDate = (checkNotNullEmpty(filter.date) ? formatDate(filter.date) : undefined);
+            $scope.showChart = false;
+            $scope.wloading = true;
+            $scope.showError = false;
+            domainCfgService.getSystemDashboardConfig().then(function (data) {
+                var domainConfig = angular.fromJson(data.data);
+                mapColors = domainConfig.mc;
+                $scope.mc = mapColors;
+                mapRange = domainConfig.mr;
+                $scope.mr = mapRange;
+                invPieColors = domainConfig.pie.ic;
+                invPieOrder = domainConfig.pie.io;
+                $scope.mapEvent = invPieOrder[0];
+                $scope.init();
+            });
+
+            $scope.init = function () {
+                domainCfgService.getMapLocationMapping().then(function (data) {
+                    if (checkNotNullEmpty(data.data)) {
+                        $scope.locationMapping = angular.fromJson(data.data);
+                        setFilters();
+                        loadLocationMap();
+                    }
+                })
+            };
+
+            function setFilters() {
+                if (checkNotNullEmpty(filter.period)) {
+                    var p = filter.period;
+                    if (p == '0' || p == '1' || p == '2' || p == '3' || p == '7' || p == '30') {
+                        $scope.period = p;
+                    }
+                } else {
+                    $scope.period = "0";
+                }
+
+                if (checkNotNullEmpty(filter.mapType)) {
+                    var mapType = filter.mapType;
+                    if (mapType == '0' || mapType == '1') {
+                        $scope.mapType = mapType;
+                        if ($scope.mapType == 0) {
+                            $scope.mapEvent = invPieOrder[0];
+                        } else {
+                            $scope.mapEvent = invPieOrder[1];
+                        }
+                    }
+                } else {
+                    $scope.mapType = "0";
+                }
+
+                if (checkNotNullEmpty(filter.materialTag)) {
+                    $scope.exFilter = constructModel(filter.materialTag);
+                    $scope.exType = 'mTag';
+                } else if (checkNotNullEmpty(filter.material)) {
+                    $scope.exFilter = filter.material.id;
+                    $scope.exType = 'mId';
+                }
+            }
+
+            function loadLocationMap() {
+                dashboardService.get(undefined, undefined, $scope.exFilter, $scope.exType, $scope.period, undefined,
+                    undefined, constructModel(filter.entityTag), fDate, constructModel(filter.exEntityTag),
+                    false).then(function (data) {
+                        $scope.dashboardView = data.data;
+                        var linkText;
+                        if ($scope.dashboardView.mLev == "country") {
+                            linkText = $scope.locationMapping.data[$scope.dashboardView.mTy].name;
+                            $scope.mapType = linkText;
+                        } else if ($scope.dashboardView.mLev == "state") {
+                            $scope.mapType = $scope.dashboardView.mTy;
+                        }
+                        constructMapData($scope.mapEvent, true, $scope, INVENTORY, $sce, mapRange, mapColors,
+                            invPieOrder, $timeout);
+                        setWidgetData();
+                    }).catch(function error(msg) {
+                        showError(msg, $scope);
+                    }).finally(function () {
+                        $scope.loading = false;
+                        $scope.wloading = false;
+                    });
+            };
+
+            function setWidgetData() {
+                $scope.eventMapWidget = {
+                    wId: $scope.widget.id,
+                    cType: $scope.mapType,
+                    copt: $scope.mapOpt,
+                    cdata: $scope.mapData,
+                    computedWidth: '100%',
+                    computedHeight: parseInt($scope.widget.computedHeight, 10) - 30,
+                    colorRange: $scope.mapRange,
+                    markers: $scope.markers
+                };
+                $scope.wloading = false;
+                $scope.showChart = true;
+            }
+        }]);
+
+logistimoApp.requires.push('logistimo.storyboard.eventMapWidget');
