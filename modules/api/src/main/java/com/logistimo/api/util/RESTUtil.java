6b524b49f3510ddd3649d4afbeea5099a479c1cb
yuvaraj <yuvaraj@logistimo.com>
2018-03-16 14:15:33 +0530

    LS-7454 Add default material tags and exclude temperature status tags configuration options in HTTP V2 Login API response

--- modules/api/src/main/java/com/logistimo/api/util/RESTUtil.java
+++ modules/api/src/main/java/com/logistimo/api/util/RESTUtil.java
@@ -1735,6 +1735,19 @@ public class RESTUtil {
       if (kioskTags != null) {
         config.put(JsonTagsZ.ENTITY_TAG, Arrays.asList(kioskTags));
       }
+      Hashtable<String, Object>
+          configDB =
+          new Hashtable<>();
+
+      // Add default material tags, if configured in the domain
+      String[] defaultMaterialTags = TagUtil.getTagsArray(dc.getDashboardConfig().getDbOverConfig().dmtg);
+      if (defaultMaterialTags != null) {
+        configDB.put(JsonTagsZ.DEFAULT_MATERIAL_TAGS, Arrays.asList(defaultMaterialTags));
+      }
+
+      if (configDB != null) {
+        config.put(JsonTagsZ.DASHBOARD, configDB);
+      }
       // Returns policy configuration
       List<ReturnsConfig> returnsConfigs = dc.getInventoryConfig().getReturnsConfig();
       if (CollectionUtils.isNotEmpty(returnsConfigs)) {
